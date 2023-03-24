78479eb706a4aedbdceef4d7b64fb3469091a3b2
Kolton D. Atwood <33530886+kdatwood@users.noreply.github.com>
2023-03-24 14:26:00 -0600

    Added FileUploader and ImageDisplay components to App.jsx, still need to figure out how to get image to display

--- AiSnap/src/App.jsx
+++ AiSnap/src/App.jsx
@@ -1,32 +1,28 @@
-import { useState } from 'react'
-import aiSnapLogo from './result.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import aiSnapLogo from "./assets/result.png";
+import FileUploader from "./components/FileUploader/FileUploader";
+import ImageDisplay from "./components/ImageDisplay/ImageDisplay";
+// import classificationResultsd from './components/ClassificationResults/ClassificationResults'  //Commented out because this doesn't export anything yet
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <div>
-        <img src={aiSnapLogo} className="logo"/>
+        <img src={aiSnapLogo} className="logo" />
+        <div className="appName">
+          <h1>AI Snap</h1>
+          <h3>Image Classification</h3>
+        </div>
       </div>
-      <h1>AI Snap</h1>
-      <h3>Image Classification</h3>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <FileUploader />
+        <ImageDisplay />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
