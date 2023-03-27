d75f16748200c13284fc772a51640295711df6ad
Kim <10829539@uvu.edu>
2023-03-27 14:49:08 -0600

    fixed app.jsx to show the button for input the image and submit button for it changed the logo of the app on the tab changed the logo name to logo

--- AiSnap/src/App.jsx
+++ AiSnap/src/App.jsx
@@ -1,26 +1,44 @@
-import { useState } from "react";
-import aiSnapLogo from "./assets/result.png";
-import FileUploader from "./components/FileUploader/FileUploader";
-import ImageDisplay from "./components/ImageDisplay/ImageDisplay";
-// import classificationResultsd from './components/ClassificationResults/ClassificationResults'  //Commented out because this doesn't export anything yet
-import "./App.css";
+import React, { useState } from 'react';
+import FileUploader from './components/FileUploader/FileUploader';
+import ImageDisplay from './components/ImageDisplay/ImageDisplay';
+import ClassificationResults from './components/ClassificationResults/ClassificationResults';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [imageSrc, setImageSrc] = useState(null);
+  const [classificationResults, setClassificationResults] = useState(null);
+
+  const handleFileSelect = (file) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      setImageSrc(e.target.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const handleSubmit = () => {
+    // Call your API for classification and update the classificationResults state here
+  };
 
   return (
-    <div className="App">
-      <div>
-        <img src={aiSnapLogo} className="logo" />
-        <div className="appName">
-          <h1>AI Snap</h1>
-          <h3>Image Classification</h3>
-        </div>
-      </div>
-      <div>
-        <FileUploader />
-        <ImageDisplay />
-      </div>
+    <div className="app">
+      <Box mb={2}>
+        <FileUploader onFileSelect={handleFileSelect} />
+      </Box>
+      <ImageDisplay imageSrc={imageSrc} />
+      <Box mt={2} mb={2}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleSubmit}
+          disabled={!imageSrc}
+        >
+          Submit Image
+        </Button>
+      </Box>
+      <ClassificationResults results={classificationResults} />
     </div>
   );
 }
