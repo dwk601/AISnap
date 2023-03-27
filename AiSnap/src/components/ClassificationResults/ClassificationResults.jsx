9d45713c0082fadfa223dc5039948f3ddfb642a7
Kim <10829539@uvu.edu>
2023-03-27 14:36:55 -0600

    added ClassificationResult.jsx for result

--- AiSnap/src/components/ClassificationResults/ClassificationResults.jsx
+++ AiSnap/src/components/ClassificationResults/ClassificationResults.jsx
@@ -1,4 +1,22 @@
 import React from 'react';
 import './ClassificationResults.css';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+
+const ClassificationResults = ({ results }) => {
+  return (
+    <div className="classification-results">
+      {results ? (
+        results.map((result, index) => (
+          <div key={index}>
+            <span>{result.label}: </span>
+            <span>{(result.probability * 100).toFixed(2)}%</span>
+          </div>
+        ))
+      ) : (
+        <p>No classification results available</p>
+      )}
+    </div>
+  );
+};
+
+
+export default ClassificationResults;
